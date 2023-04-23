881f806ebc137fb5dae2f533a9752920ae2a4f86
Ibrat Zayniddinov <tony@Ibrats-Mac-mini.local>
2023-04-23 21:56:12 +0500

    templates are done and registration redux installed

--- base/urls.py
+++ base/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls import static
+from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('user', include('user.UserUniModel'))
+    path('', include('user.urls')),
 ]
 
 
